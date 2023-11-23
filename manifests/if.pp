a8e05873295913be4426a01dfdd8e732725b0357
john275 <john.ballantyne@mailonline.co.uk>
2023-11-23 12:31:31 +0000

    refactor

--- manifests/if.pp
+++ manifests/if.pp
@@ -1,13 +1,14 @@
-if $facts['os']['family'] == 'RedHat' {
-  package { 'httpd':
-    ensure => installed,
-  }
-} elsif $facts['os']['family'] == 'Debian' {
-  package { 'apache2':
-    ensure => installed,
-  }
-} else {
-  notify { 'Unsupported OS':
-    message => "The operating system ${facts['os']['family']} is not supported.",
-  }
-}
+#if $facts['os']['family'] == 'RedHat' {
+#  package { 'httpd':
+#    ensure => installed,
+#  }
+#} elsif $facts['os']['family'] == 'Debian' {
+#  package { 'apache2':
+#    ensure => installed,
+#  }
+#} else {
+#  notify { 'Unsupported OS':
+#    message => "The operating system ${facts['os']['family']} is not supported.",
+#  }
+#}
+#
