6584354f61bf53c60e8204a89efee62c4543289d
john275 <john.ballantyne@mailonline.co.uk>
2023-11-23 16:42:33 +0000

    remove httpd

--- manifests/case.pp
+++ manifests/case.pp
@@ -1,26 +1,27 @@
-$osfamily1 = $facts['os']['family']
-case $osfamily1 {
-  'RedHat', 'CentOS': {
-    package { 'httpd':
-      ensure => installed,
-    }
-    service { 'httpd':
-      ensure => running,
-      enable => true,
-    }
-  }
-  'Debian', 'Ubuntu': {
-    package { 'apache2':
-      ensure => installed,
-    }
-    service { 'apache2':
-      ensure => running,
-      enable => true,
-    }
-  }
-  default: {
-    notify { 'Unsupported OS':
-      message => "The operating system ${osfamily1} is not supported.",
-    }
-  }
-}
+#$osfamily1 = $facts['os']['family']
+#case $osfamily1 {
+#  'RedHat', 'CentOS': {
+#    package { 'httpd':
+#      ensure => installed,
+#    }
+#    service { 'httpd':
+#      ensure => running,
+#      enable => true,
+#    }
+#  }
+#  'Debian', 'Ubuntu': {
+#    package { 'apache2':
+#      ensure => installed,
+#    }
+#    service { 'apache2':
+#      ensure => running,
+#      enable => true,
+#    }
+#  }
+#  default: {
+#    notify { 'Unsupported OS':
+#      message => "The operating system ${osfamily1} is not supported.",
+#    }
+#  }
+#}
+#
