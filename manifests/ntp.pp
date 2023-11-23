cd1ae93e159d274d61d3af0f7cd46d1c8d57d8de
john275 <john.ballantyne@mailonline.co.uk>
2023-11-23 10:13:19 +0000

    ntpd

--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -3,15 +3,14 @@ ensure  => file,
 content => "driftfile /var/lib/ntp/drift\nrestrict default nomodify notrap nopeer noquery\nrestrict 127.0.0.1 \nrestrict ::1\nserver 0.centos.pool.ntp.org iburst\nserver 1.centos.pool.ntp.org iburst\nserver 2.centos.pool.ntp.org iburst\nserver 3.centos.pool.ntp.org iburst\nincludefile /etc/ntp/crypto/pw\nkeys /etc/ntp/keys\ndisable monitor",
 #content => template('module_name/ntp.conf.erb'), # Assuming you have a template for NTP configuration
 # Notify the NTP service to restart when the configuration file changes
- notify  => Service['ntp'],
+ notify  => Service['ntpd'],
 }
 
 # Define the service resource for the NTP service
-service { 'ntp':
+service { 'ntpd':
 ensure     => running,
   enable     => true,
   hasrestart => true,
-  provider => "redhat",
   # Subscribe to changes in the NTP configuration file
   subscribe => File['/etc/ntp.conf'],
 }
